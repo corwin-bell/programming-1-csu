1e359ba85c19bf6714e1465c021875a9fbae0eba
corwin-bell <corwin.m.bell@gmail.com>
2024-05-15 11:46:42 -0600

    comment clean upand execution screenshot

--- module1/src/addressPrinter.java
+++ module1/src/addressPrinter.java
@@ -2,26 +2,19 @@
 public class addressPrinter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//		initialize and assign values to string fields:
-//			firstName
-//			lastName
-//			streetAddress
-//			city
-//			zipCode
+		
+		// initialize address fields
 		String firstName = "Joe";
 		String lastName = "Smith";
 		String streetAddress = "123 Main St";
 		String city = "Littleton";
 		String zipCode = "12345";
 			
-//		for field in field list:
-//			print on newline (field)
+		// print address fields		
 		System.out.println("First Name: "+ firstName);
 		System.out.println("Last Name: "+ lastName);
 		System.out.println("Street Address: "+ streetAddress);
 		System.out.println("City: "+ city);
 		System.out.println("Zip Code: "+ zipCode);
 	}
-
 }
