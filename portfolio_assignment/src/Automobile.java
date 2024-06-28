e05e07985b6774a45092a62c608a748cb8e511de
corwin-bell <corwin.m.bell@gmail.com>
2024-06-28 13:53:07 -0700

    basic program interface and addAuto method

--- portfolio_assignment/src/Automobile.java
+++ portfolio_assignment/src/Automobile.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Automobile {
     private String ID;
     private String make;
@@ -5,7 +8,9 @@ public class Automobile {
     private String color;
     private int year;
     private int mileage;
+    // private ArrayList<String> fieldList = new ArrayList<>(Arrays.asList("make","model","color","year","mileage"));
      
+    // add try catch blocks to most appropriate place, here or reference class?
     public Automobile() {
         ID = "0"; //FIXME: add auto-increment value
         make = "unknown";
@@ -17,22 +22,27 @@ public class Automobile {
 
     public Automobile(String make, String model, String color, int year, int mileage) {
         // FIXME: implement parametarized constructor
-        // ID = "0";
-        // this.make = make;
-        // this.model 
-        // this.color
-        // this.year
-        // this.mileage
+        ID = "0";
+        this.make = make;
+        this.model = model; 
+        this.color = color;
+        this.year = year;
+        this.mileage = mileage;
     }
     // update vehicle attributes methods
     public void setMake(String make) {
         this.make = make;
     }
+
+    // set all as method in class or reference class?
     // FIXME: list all atrtributes
     public String getMake() {return make;}
-    
+    // public ArrayList<String> getFieldList() {return fieldList;}
     // list vehicle information (return String array)
+    // redundant with get methods? maybe best to have in
+    // reference class?  
     public String getautoInfo() {
+        // try hashmap
         return "FIXME: list all auto info as String array";
     }
 }
--- portfolio_assignment/src/Automobile_Inventory.java
+++ portfolio_assignment/src/Automobile_Inventory.java
@@ -2,25 +2,75 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.InputMismatchException;
 
 public class Automobile_Inventory {
     
-    // declare variables
+    // declare variable
     
     public static void main(String[] args) throws Exception {
+        // consider refactor as linkedlist
         ArrayList<Automobile> autoList = new ArrayList<>();
-        // add while loop for user input
-        autoList.add(new Automobile());
-        // test to make sure Automobile object added to list
-        System.out.println(autoList.get(0).getMake());
+        String command = "";
+        try (Scanner scnr = new Scanner(System.in)) { // try with Scanner to ensure it closes
+            do {
+                System.out.println("Commands: 'addAuto', 'listAutoInventory', 'removeAuto', 'updateAuto','exportInventory', 'quit'");
+                command = scnr.nextLine();
+                if (command.equalsIgnoreCase("addAuto")) {
+                    addAuto(scnr, autoList);
+                }
+            }
+            while (!command.equalsIgnoreCase("quit"));   
+
+        }
+            // try-catch for user commands
+                // addAuto()
+                // listAutoInventory()
+                // removeAuto()
+                // addAuto()
+                // updateAuto()
+                // exportInventory()
     }
     
-    public static void addAuto() {
-            //FIXME: ask user for auto attribute input
+    public static void addAuto(Scanner scnr, ArrayList<Automobile> autoList) {
+        // add new auto to autoList inventory
+        try {
+            System.out.println("Enter make(String)");
+            String make = scnr.next();
+            System.out.println("Enter model(String)");
+            String model = scnr.next();
+            System.out.println("Enter color(String)");
+            String color = scnr.next();
+            System.out.println("Enter year(int)");
+            int year = scnr.nextInt();
+            System.out.println("Enter mileage(int)");
+            int mileage = scnr.nextInt();
+            
+            Automobile auto = new Automobile(make, model, color, year, mileage);
+            autoList.add(auto);
+            System.out.println(autoList.get(0).getMake());
+            System.out.println("auto added");
+        }
+        catch (InputMismatchException e) {
+            System.out.println("must enter correct data type for each field");
+            scnr.nextLine(); // clears scanner before next userInput
+            // FIXME: route user to failed input attempt
+        }
         }
     
-    public static void removeAuto() {
-            //FIXME: remove auto based on ID from user input
-        }
+    public static void listAutoInventory() {
+
+    }
+    
+    public static void removeAuto() { // remove auto from inventory based on ID from user input
+        // try
+        //     remove Automobile from autoList
+        //     print success message
+        // catch value not found
+        //     print error message
+    }
+    
+    public static void exportInventory() { // print information to file
+    }
     
 }
